8bc47e0d60d753c050e298f115547e1c1c705b45
apple <apple@apples-MacBook-Pro-2.local>
2022-07-10 23:54:06 +0100

    create username for each account

--- script.js
+++ script.js
@@ -80,6 +80,19 @@ const displayMovements = function (movements){
 
 displayMovements(account1.movements);
 
+// create innitials for log in
+
+const createUsernames = function(accs){
+    accs.forEach((acc)=>{
+        acc.username = acc.owner.toLowerCase().split(' ').map((name => name[0])).join('');
+        
+    })
+}
+
+createUsernames(accounts);
+console.log(accounts)
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
